821df664c78b0fac2eddf32de2f9ef5986962869
Mike <jogdial25@gmail.com>
2025-03-04 12:11:54 +0200

    example data instead hardcoded data

--- src/ProjectData.ts
+++ src/ProjectData.ts
@@ -1,26 +1,36 @@
 const projects = [
     {
-        id: 1,
-        name: 'Project 1',
-        description: 'This is a project description',
-        tasks: [
-        {
-            id: 1,
-            name: 'Task 1',
-            description: 'This is a task description'
-        }
-        ]
+      id: 1,
+      name: "Paide Kindergarten",
+      image: "project-1.webp",
+      intro: "Paide kindergarten",
+      details: {
+        techLogos: ["wordpress.svg", "elementorLogo.svg"],
+        techDescription: "Created with WordPress & Elementor",
+      },
+      liveLink: "#", // Replace with actual live link if available
     },
     {
-        id: 2,
-        name: 'Project 2',
-        description: 'This is a project description',
-        tasks: [
-        {
-            id: 1,
-            name: 'Task 1',
-            description: 'This is a task description'
-        }
-        ]
-    }
-]
+      id: 2,
+      name: "Estonian Agility Competition Calendar",
+      video: "calendar.mp4",
+      intro: "Estonian Agility competition calendar",
+      details: {
+        techLogos: [
+          "js-logo.svg",
+          "react-logo.svg",
+          "vite-logo.svg",
+          "php-logo.svg",
+          "mysql-logo.svg",
+          "html-5.svg",
+          "css-3.svg",
+          "instagram-logo.webp",
+        ],
+        techDescription: "Including Instagram post API integration on news page",
+      },
+      liveLink: "#",
+    },
+  ];
+  
+  export default projects;
+  
