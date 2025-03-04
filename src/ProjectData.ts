6b5c00b44b8180b23e111b5c06c771673a9c7be4
Mike <jogdial25@gmail.com>
2025-03-04 12:09:42 +0200

    instead using hardcoded data include data from ProjectData file it example

---
+++ src/ProjectData.ts
@@ -0,0 +1,26 @@
+const projects = [
+    {
+        id: 1,
+        name: 'Project 1',
+        description: 'This is a project description',
+        tasks: [
+        {
+            id: 1,
+            name: 'Task 1',
+            description: 'This is a task description'
+        }
+        ]
+    },
+    {
+        id: 2,
+        name: 'Project 2',
+        description: 'This is a project description',
+        tasks: [
+        {
+            id: 1,
+            name: 'Task 1',
+            description: 'This is a task description'
+        }
+        ]
+    }
+]
