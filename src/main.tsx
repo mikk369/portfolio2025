a887ebb3065d9f176ecd9aaecf77a6c8dc2d16b9
Mike <jogdial25@gmail.com>
2025-03-04 13:44:46 +0200

    improted css files to main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import './App.css'
+import './general.css'
+import './mediaQuerys.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
